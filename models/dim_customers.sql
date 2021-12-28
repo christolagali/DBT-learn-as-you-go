b1871838df85532f09ed350e1e35f7984576f7cd
admin <admin@Livinglys-MacBook-Pro-2.local>
2021-12-28 17:16:22 -0500

    created and deployed the first customers dim view

---
+++ models/dim_customers.sql
@@ -0,0 +1,41 @@
+with customers_cte as (
+  
+  select 
+  id as customer_id,
+  first_name,
+  last_name
+  from DBT_DB.DEV.CUSTOMERS
+
+),
+orders_cte as(
+  
+  select
+  id as order_id,
+  user_id as customer_id,
+  order_date,
+  status
+  from DBT_DB.DEV.ORDERS
+
+),
+customer_orders as(
+  
+  select customer_id,
+  min(order_date) as first_order_date,
+  max(order_date) as most_recent_order_date,
+  count(order_id) as number_of_orders
+  from orders_cte
+  group by 1
+),
+final as(
+  
+  select 
+    customers_cte.customer_id,
+    customers_cte.first_name,
+    customers_cte.last_name,
+  customer_orders.first_order_date,
+  customer_orders.most_recent_order_date,
+  coalesce(customer_orders.number_of_orders,0) as number_of_orders
+  from customers_cte
+  left join customer_orders using(customer_id)
+
+)select * from final
