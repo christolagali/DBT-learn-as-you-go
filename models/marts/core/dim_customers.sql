f44dfe28aac1c21bea34c5567114d64bb204b426
admin <admin@Livinglys-MacBook-Pro-2.local>
2021-12-31 15:35:17 -0500

    Refactored Code for dim_customers code

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -4,14 +4,15 @@ with customers_cte as(
 ),
 orders_cte as(
     select *
-    from {{ ref('stg_orders') }}
+    from {{ ref('fct_orders') }}
 ),
 customer_orders as(
   
   select customer_id,
   min(order_date) as first_order_date,
   max(order_date) as most_recent_order_date,
-  count(order_id) as number_of_orders
+  count(order_id) as number_of_orders,
+  sum(amount) as lifetime_value
   from orders_cte
   group by 1
 ),
@@ -23,7 +24,8 @@ final as(
     customers_cte.last_name,
   customer_orders.first_order_date,
   customer_orders.most_recent_order_date,
-  coalesce(customer_orders.number_of_orders,0) as number_of_orders
+  coalesce(customer_orders.number_of_orders,0) as number_of_orders,
+  customer_orders.lifetime_value
   from customers_cte
   left join customer_orders using(customer_id)
 
